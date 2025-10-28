5b4667b9e3cd2b64094534d3d4b38ffcdc36143a
SimonteBiesebeek <simonchristiaan.tebiesebeek@uzh.ch>
2025-10-28 19:24:01 +0100

    fixed error

--- FinalProjectPython.py
+++ FinalProjectPython.py
@@ -6,8 +6,6 @@ from pathlib import Path
 
 st.title("Final Project: Data Analysis and Visualization")
 
-st.header("Data")
-
 ROOT = Path(__file__).parent
 df_merged = pd.read_csv(ROOT / "MergedDutchLeagueData.csv")
 df_merged = pd.read_csv("MergedDutchLeagueData.csv")
