3f409d6d99c9a1c3787b75a5b4db02a25326181b
SimonteBiesebeek <simonchristiaan.tebiesebeek@uzh.ch>
2025-10-28 19:07:28 +0100

    added data

--- FinalProjectPython.py
+++ FinalProjectPython.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
 st.title("Final Project: Data Analysis and Visualization")
 
 st.header("Data")
+
+df_merged = pd.read_csv("MergedDutchLeagueData.csv")
