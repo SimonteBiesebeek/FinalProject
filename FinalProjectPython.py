699ceb21029e194ccfd8f9bc480ec4bf426f2c3b
SimonteBiesebeek <simonchristiaan.tebiesebeek@uzh.ch>
2025-10-28 19:23:22 +0100

    fixed error

--- FinalProjectPython.py
+++ FinalProjectPython.py
@@ -9,6 +9,9 @@ st.title("Final Project: Data Analysis and Visualization")
 st.header("Data")
 
 ROOT = Path(__file__).parent
+df_merged = pd.read_csv(ROOT / "MergedDutchLeagueData.csv")
+df_merged = pd.read_csv("MergedDutchLeagueData.csv")
+
 
 st.header("Project Introduction")
 
@@ -23,3 +26,4 @@ with col_data:
 with col_chart:
     st.subheader("Data Overview")
     st.markdown("Placeholder for chart")
+    
