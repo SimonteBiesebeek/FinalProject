217a39062b8af75d56ea7cb6ff178dfb55fd56ba
SimonteBiesebeek <simonchristiaan.tebiesebeek@uzh.ch>
2025-10-28 19:15:42 +0100

    relative path

--- FinalProjectPython.py
+++ FinalProjectPython.py
@@ -2,10 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
+
 st.title("Final Project: Data Analysis and Visualization")
 
 st.header("Data")
 
+ROOT = Path(__file__).parent
+df_merged = pd.read_csv(ROOT / "MergedDutchLeagueData.csv")
 df_merged = pd.read_csv("MergedDutchLeagueData.csv")
 
 st.dataframe(df_merged.head())
